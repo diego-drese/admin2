394e03d5f38b56d18bc6c0eff69f9545d541134b
luan <luan@beholder.local>
2018-08-10 09:56:42 -0300

    ajustado o iframe e css do logs

--- src/resources/views/backend/logview/index.blade.php
+++ src/resources/views/backend/logview/index.blade.php
@@ -1,1 +1,15 @@
-<iframe src="/log-viewer" width="100%" height="100%"></iframe>
+@extends('Ironforge::layouts.backend.main')
+@section('title', 'Logs')
+@section('content')
+
+    <div class="content-wrapper">
+        @include('Ironforge::layouts.backend.breadcrumb')
+
+        <section class="content">
+            <div style="height:900px;">
+                <iframe src="/log-viewer" width="100%" height="100%" style="border:0px"></iframe>
+                <div class="clearfix"></div>
+            </div>
+        </section>
+    </div>
+@endsection

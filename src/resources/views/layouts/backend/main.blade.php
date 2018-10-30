e75e326e3c06a39f217b5875a0cda0394f404ae3
ramirespoa <pablo.ramires@aggregando.com.br>
2018-10-30 11:20:38 -0300

    [HotFix] add https to the url

--- src/resources/views/layouts/backend/main.blade.php
+++ src/resources/views/layouts/backend/main.blade.php
@@ -8,7 +8,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     @yield('style')
     <link href="/vendor/aggrega/ironforge/laravel-package-ironforge/css/app.css" type="text/css" rel="stylesheet">
-    <script src="//use.edgefonts.net/kaushan-script.js"></script>
+    <script src="https://use.edgefonts.net/kaushan-script.js"></script>
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>

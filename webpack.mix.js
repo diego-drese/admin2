663503c6b8791454b65212b2d12f16b2dae0ff6a
ramirespoa <pablo.ramires@aggregando.com.br>
2018-11-06 14:42:48 -0200

    [CAMPAIGN] add campaign manager

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,8 +8,11 @@ mix.babel([
     'src/resources/assets/theme/backend/js/app.min.js',
     'src/resources/assets/theme/backend/js/script.js',
     'src/resources/assets/theme/backend/js/toastr.js',
-    'src/resources/assets/theme/backend/plugins/iCheck/icheck.min.js',
+	'src/resources/assets/theme/backend/plugins/iCheck/icheck.min.js',
     'src/resources/assets/js/Controller.js',
+	'src/resources/assets/theme/backend/js/ion.rangeSlider.js',
+	'src/resources/assets/theme/backend/js/jquery.inputmask.bundle.min.js',
+	'src/resources/assets/theme/backend/js/bootstrap-editable.min.js',
 ],'src/public/js/app.js').version();
 
 mix.sass('src/resources/assets/theme/backend/css/sass/ironforge.scss', 'src/public/css/ironforge.css');
@@ -22,6 +25,7 @@ mix.styles([
     'src/public/css/ironforge.css',
     'src/resources/assets/theme/backend/css/ironforge-login.css',
     'src/resources/assets/theme/backend/plugins/iCheck/square/blue.css',
+	'src/resources/assets/theme/backend/css/ion.rangeSlider.css',
 ],'src/public/css/app.css').version();
 
 
@@ -30,6 +34,7 @@ mix.copy('src/resources/assets/theme/backend/img/login.jpg', 'src/public/img');
 mix.copy('src/resources/assets/js/pt-br.json', 'src/public/js');
 
 mix.copy('src/resources/assets/theme/backend/img/favicon.png', 'src/public/img');
+mix.copy('src/resources/assets/theme/backend/img/sprite-skin-flat.png', 'src/public/img');
 
 mix.copyDirectory('src/resources/assets/theme/backend/css/fonts', 'src/public/fonts');
 

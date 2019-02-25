5c5e53bbf8149bb6cc4cdc75d2f85cf5b1932835
Diego Neumann <diego-neumann@hotmail.com>
2019-02-25 20:14:18 -0300

    Ajustado conexao

--- src/config/database.php
+++ src/config/database.php
@@ -1,8 +1,22 @@
 <?php
-
+/**
+ * Created by PhpStorm.
+ * User: diego
+ * Date: 15/08/18
+ * Time: 15:10
+ */
 return [
-    'prefix_url' => env('NEGOTIATE_ADMIN_PREFIX_URL', 'adm'),
-    'favicon' => env('NEGOTIATE_ADMIN_FAVICON', '/vendor/negotiate/admin/img/favicon.png'),
-    'logo_label' => env('NEGOTIATE_ADMIN_LOGO_LABEL', 'Negotiate'),
-    'owner_type' => [],
+    'negotiate_admin' => [
+        'driver' => 'mongodb',
+        'host' => env('NEGOTIATE_ADMIN_DB_HOST', '127.0.0.1'),
+        'port' => env('NEGOTIATE_ADMIN_DB_PORT', '27017'),
+        'database' => env('NEGOTIATE_ADMIN_DB_NAME', 'negotiate_admin'),
+        'username' => env('NEGOTIATE_ADMIN_DB_USERNAME', ''),
+        'password' => env('NEGOTIATE_ADMIN_DB_PASSWORD', ''),
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+        'strict' => false,
+        'engine' => null
+    ]
 ];

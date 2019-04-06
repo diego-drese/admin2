d8581118a692d30ac8c5bdeeeccad08201f1002f
Diego Neumann <diego-neumann@hotmail.com>
2019-04-06 13:04:52 -0300

    retirado tema quillpro

--- src/resources/views/errors/403.blade.php
+++ src/resources/views/errors/403.blade.php
@@ -1,17 +1,23 @@
 @if(Auth::check())
-    @extends('Admin::layouts.quillpro.main')
-    @section('title', 'Page not Allowed')
+    @extends('Admin::layouts.backend.main')
+    @section('title', 'Page not Found')
     @section('content')
-        <div class="col-md-12 my-5 text-center">
-            <div class="text-danger">
-                <i class="batch-icon batch-icon-link-alt batch-icon-xxl"></i>
-                <i class="batch-icon batch-icon-search batch-icon-xxl"></i>
-                <i class="batch-icon batch-icon-link-alt batch-icon-xxl"></i>
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-12 my-5 text-center">
+                        <div class="text-danger">
+                            <i class="batch-icon batch-icon-link-alt batch-icon-xxl"></i>
+                            <i class="batch-icon batch-icon-search batch-icon-xxl"></i>
+                            <i class="batch-icon batch-icon-link-alt batch-icon-xxl"></i>
+                        </div>
+                        <h1 class="display-1">403</h1>
+                        <div class="display-4 mb-3">Page Not Allowed</div>
+                        <div class="lead">We can't find the page you are looking for.</div>
+                        <div class="lead">Try searching for it using the search field below or you can go <a href="/">back to our homepage</a>.</div>
+                    </div>
+                </div>
             </div>
-            <h1 class="display-1">403</h1>
-            <div class="display-4 mb-3">Page Not Allowed</div>
-            <div class="lead">We can't find the page you are looking for.</div>
-            <div class="lead">Try searching for it using the search field below or you can go <a href="/">back to our homepage</a>.</div>
         </div>
     @endsection
 @endif

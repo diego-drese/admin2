f0dfcce8ce8c010a8e631544857220b0c31d7579
Diego Neumann <diego-neuamnn@hotmail.com>
2019-09-27 18:14:15 -0300

    Ajustado names

--- src/config/admin.php
+++ src/config/admin.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'prefix_url'            => env('NEGOTIATE_ADMIN_PREFIX_URL', 'adm'),
-    'favicon'               => env('NEGOTIATE_ADMIN_FAVICON', '/vendor/oka6/admin/img/favicon.png'),
-    'logo_label'            => env('NEGOTIATE_ADMIN_LOGO_LABEL', 'Oka6'),
+    'prefix_url'            => env('OKA6_ADMIN_PREFIX_URL', 'adm'),
+    'favicon'               => env('OKA6_ADMIN_FAVICON', '/vendor/oka6/admin/img/favicon.png'),
+    'logo_label'            => env('OKA6_ADMIN_LOGO_LABEL', 'Oka6'),
     "cache_prefix"          => 'oka6',
     'profile_type'          => ['admin' => 'Admin'],
     'plan_fields_update'    => [],

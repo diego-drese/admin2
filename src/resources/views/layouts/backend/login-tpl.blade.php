52092004d4064d871d4282e197fb5a2e1db6f28d
luan <you@example.com>
2020-03-15 19:18:16 -0300

    ajuste icon

--- src/resources/views/layouts/backend/login-tpl.blade.php
+++ src/resources/views/layouts/backend/login-tpl.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Login</title>
-    <link rel="icon" type="image/png" sizes="16x16" href="/vendor/oka6/admin/assets/images/favicongi.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/vendor/oka6/admin/assets/images/favicon.png">
 
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link rel="stylesheet" href="{{mix('/vendor/oka6/admin/css/basic.css')}}">
